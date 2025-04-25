0258a044ad000597e8f4985cbdf40bb1ceb980c7
Walter Melo <41174751+walmello@users.noreply.github.com>
2025-04-25 02:13:39 -0300

    now it's SSR?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
